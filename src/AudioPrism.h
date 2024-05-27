8038abbe7df5720a8ddec669cca7cb76af183a9b
grant-tm <grant.t.morgan@gmail.com>
2024-05-26 19:44:48 -0700

    Create AudioPrism.h to include entire library

---
+++ src/AudioPrism.h
@@ -0,0 +1,17 @@
+#ifndef Audio_Prism_h
+#define Audio_Prism_h
+
+#include "AnalysisModule.h"
+
+#include "modules/BreadSlicer.h"
+#include "modules/Centroid.h"
+#include "modules/DeltaAmplitudes.h"
+#include "modules/MajorPeaks.h"
+#include "modules/MaxAmplitude.h"
+#include "modules/MeanAmplitude.h"
+#include "modules/Noisiness.h"
+#include "modules/PercussionDetection.h"
+#include "modules/SalientFreqs.h"
+#include "modules/TotalAmplitude.h"
+
+#endif // Audio_Prism_h
