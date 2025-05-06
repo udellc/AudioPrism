26de80ddf9395ae9a4c03c25ffa1a2afe53526fc
Ashton Tilton <tiltona@oregonstate.edu>
2025-05-06 08:36:15 -0700

    Added Deltas

--- src/modules/AmplitudeTools.h
+++ src/modules/AmplitudeTools.h
@@ -2,10 +2,12 @@
 #define Amplitude_Tools_h 
 
 #include "../AnalysisModule.h"
+#include <cmath>
+#include <vector>
 
 class AmplitudeTools {
     public:
-    //Sum of Amplitudes
+    //Sum of Amplitudes in lowerBinBound, upperBinBound
     static float total(const float** input, int lowerBinBound, int upperBinBound) {
         float total = 0.0f;
         for (int i = lowerBinBound; i < upperBinBound; ++i) {
@@ -14,13 +16,13 @@ class AmplitudeTools {
         return total;
     }
 
-    //Mean of amplitudes
+    //Mean of amplitudes in lowerBinBound, upperBinBound
     static float mean(const float** input, int lowerBinBound, int upperBinBound) {
         float t = total(input, lowerBinBound, upperBinBound);
         return t / float(upperBinBound - lowerBinBound);
     }
 
-    //Max amplitude
+    //Max amplitude in lowerBinBound, upperBinBound
     static float max(const float **input, int lowerBinBound, int upperBinBound) {
         float maxVal = 0.0f;
         for (int i = lowerBinBound; i < upperBinBound; ++i) {
@@ -30,6 +32,25 @@ class AmplitudeTools {
         }
         return maxVal;
     }
+
+    //Sum of absolute delta amplitudes between current and previous window
+    static float delta(const float **input, int lowerBinBound, int upperBinBound) {
+        float sumDelta = 0.0f
+        for (int i = lowerBinBound; i < upperBinBound;++i) {
+            sumDelta += std::abs(input[CURR_WINDOW][i] - input[PREV_WINDOW][i]);
+        }
+        return sumDelta; 
+    }
+
+    //Per-bin delta amplitudes between current and previous window
+    static std::vector<float> deltaArray(const flaot** input, int lowerBinBound, int upperBinBound) {
+        int size = upperBinBound - lowerBinBound;
+        std::vector<float> deltas(size);
+        for (int i  = lowerBinBound; i < upperBinBound; ++i) {
+            deltas[i - lowerBinBound] = std::abs(input[CURR_WINDOW][i] - input[PREV_WINDOW][i]);
+        }
+        return deltas;
+    }
 }; 
 
-#endif
+#endif //AMPLITUDE_TOOLS_H
